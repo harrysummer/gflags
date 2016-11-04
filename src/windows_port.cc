e2040595f9072eef65b8c502fffabf6910dead76
Harry Summer <harrysummerxr@gmail.com>
2016-11-04 21:25:01 +0800

    fix compiling issue with VS2015 and MSYS2

--- src/windows_port.cc
+++ src/windows_port.cc
@@ -59,6 +59,7 @@ int safe_vsnprintf(char *str, size_t size, const char *format, va_list ap) {
 #  pragma warning(pop)
 #endif
 
+#if _MSC_VER < 1900
 int snprintf(char *str, size_t size, const char *format, ...) {
   int r;
   va_list ap;
@@ -67,5 +68,6 @@ int snprintf(char *str, size_t size, const char *format, ...) {
   va_end(ap);
   return r;
 }
+#endif
 
 #endif  /* #if !defined(__MINGW32__) && !defined(__MINGW64__) */
